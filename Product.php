f75414427a5085dd5cdc35e0a5c8337bc99e7c11
Sunnatillo <97885433+sunnatillo91@users.noreply.github.com>
2023-07-31 18:46:50 +0500

    Models added

---
+++ Product.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\Translatable\HasTranslations;
+
+class Product extends Model
+{
+    use HasFactory, HasTranslations;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'price',
+        'description'
+    ];
+
+    public $translatable = ["name", "description"];
+    
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function stocks():HasMany {
+        return $this->hasMany(Stock::class);
+    }
+
+    public function users():BelongsToMany {
+
+        return $this->belongsToMany(User::class);
+    }
+}
---
+++ User.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'phone'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function roles():BelongsToMany {
+
+        return $this->belongsToMany(Role::class);
+    }
+    
+    public function favorites():BelongsToMany {
+
+        return $this->belongsToMany(Product::class);
+    }
+    
+    public function hasFavorite($favorite_id) 
+    {
+        return $this->favorites()->where('product_id', $favorite_id)->exists();
+    }
+}
